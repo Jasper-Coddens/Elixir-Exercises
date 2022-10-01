7e6e94acf6e0ef14d40e481e4b791b22aafc17b9
Jasper-Coddens <71775592+Jasper-Coddens@users.noreply.github.com>
2022-10-01 15:04:26 +0200

    working atoms trying to optimalise

---
+++ elixir_basics_reprise_exercises/12_atoms/student.exs
@@ -0,0 +1,21 @@
+defmodule Cards do
+
+  def higher?(:ace, :king), do: true
+  def higher?(:king, :ace), do: false
+  def higher?(:king, :queen), do: true
+  def higher?(:queen, :king), do: false
+  def higher?(:queen, :ace), do: false
+  def higher?(:queen, :jack), do: true
+  def higher?(:jack, :ace), do: false
+  def higher?(:jack, :king), do: false
+
+
+
+  def higher?(x,y) do
+    if x > y do
+      true
+    else
+      false
+    end
+  end
+end
